27660164068d4f07e5c018472c0075c34ff72e27
chintan chauhan <chintanchauhan1129@gmail.com>
2026-03-29 09:37:51 +0530

    Removed backend

--- server/routes/Contact.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { contactUsController } = require("../Controllers/ContactUs")
-
-router.post("/contact", contactUsController)
-
-module.exports = router
--- server/routes/mail.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { mailController } = require("../Controllers/mail")
-
-router.post("/mail", mailController)
-
-module.exports = router
